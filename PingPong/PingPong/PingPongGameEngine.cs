c05cce0eb5207119becc22413d368eda4d8039ab
Tsvetelin91 <cv.dilov@abv.bg>
2023-02-27 19:07:20 +0200

    Removing the scroll bars

--- PingPong/PingPong/PingPongGameEngine.cs
+++ PingPong/PingPong/PingPongGameEngine.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PingPong
+﻿namespace PingPong
 {
-    internal class PingPongGameEngine
+    public class PingPongGameEngine
     {
+        public void Play()
+        {
+            var firstPlayerBoardSize = 4;
+            var secondPlayerBoardSize = 4;
+            var ballPositionX = 0;
+            var ballPositionY = 0;
+            var firstPlayerPosition = 0;
+            var secondPlayerPosition = 0;
+
+            RemoveScrollBars();
+
+            while (true)
+            {
+
+
+                Thread.Sleep(70);
+            }
+        }
+
+        private static void RemoveScrollBars()
+        {
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
+        }
     }
 }
--- PingPong/PingPong/Program.cs
+++ PingPong/PingPong/Program.cs
@@ -1,12 +1,10 @@
-﻿internal class Program
+﻿using PingPong;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.BufferHeight = Console.WindowHeight;
-        Console.BufferWidth = Console.WindowWidth;
-        while (true)
-        {
-
-        }
+        var gameEngine = new PingPongGameEngine();
+        gameEngine.Play();
     }
 }
