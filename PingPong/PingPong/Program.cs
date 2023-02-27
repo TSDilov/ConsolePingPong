8221385deaaaba4e210e95aff52cb06c6558c364
Tsvetelin91 <cv.dilov@abv.bg>
2023-02-27 18:58:18 +0200

    Creating the Game engine class

---
+++ PingPong/PingPong/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BufferHeight = Console.WindowHeight;
+        Console.BufferWidth = Console.WindowWidth;
+        while (true)
+        {
+
+        }
+    }
+}
