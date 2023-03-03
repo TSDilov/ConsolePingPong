a60486f30a99516cb06781ef7b0dabc9082e755a
Tsvetelin91 <cv.dilov@abv.bg>
2023-03-03 17:19:47 +0200

    Moving the ball

--- PingPong/PingPong/Controller.cs
+++ PingPong/PingPong/Controller.cs
@@ -56,5 +56,75 @@ namespace PingPong
 
             return secondPlayerPosition;
         }
+
+        public static void MoveBall(int firstPlayerBoardSize, int secondPlayerBoardSize, ref int ballPositionX, ref int ballPositionY, int firstPlayerPosition, int secondPlayerPosition, ref int firstPlayerResult, ref int secondPlayerResult, ref bool ballDirectionUp, ref bool ballDirectionRight)
+        {
+            if (ballPositionY == 0)
+            {
+                ballDirectionUp = false;
+            }
+            if (ballPositionY == Console.WindowHeight - 1)
+            {
+                ballDirectionUp = true;
+            }
+            if (ballPositionX == Console.WindowWidth - 1)
+            {
+                ballPositionX = Console.WindowWidth / 2;
+                ballPositionY = Console.WindowHeight / 2;
+                ballDirectionRight = false;
+                ballDirectionUp = true;
+                firstPlayerResult++;
+                Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+                Console.WriteLine("First player wins!");
+                Console.ReadKey();
+            }
+            if (ballPositionX == 0)
+            {
+                ballPositionX = Console.WindowWidth / 2;
+                ballPositionY = Console.WindowHeight / 2;
+                ballDirectionRight = true;
+                ballDirectionUp = true;
+                secondPlayerResult++;
+                Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+                Console.WriteLine("Second player wins!");
+                Console.ReadKey();
+            }
+
+            if (ballPositionX < 3)
+            {
+                if (ballPositionY >= firstPlayerPosition
+                    && ballPositionY < firstPlayerPosition + firstPlayerBoardSize)
+                {
+                    ballDirectionRight = true;
+                }
+            }
+
+            if (ballPositionX >= Console.WindowWidth - 3 - 1)
+            {
+                if (ballPositionY >= secondPlayerPosition
+                    && ballPositionY < secondPlayerPosition + secondPlayerBoardSize)
+                {
+                    ballDirectionRight = false;
+                }
+            }
+
+            if (ballDirectionUp)
+            {
+                ballPositionY--;
+            }
+            else
+            {
+                ballPositionY++;
+            }
+
+            if (ballDirectionRight)
+            {
+                ballPositionX++;
+            }
+            else
+            {
+                ballPositionX--;
+            }
+        }
     }
 }
--- PingPong/PingPong/PingPongGameEngine.cs
+++ PingPong/PingPong/PingPongGameEngine.cs
@@ -12,6 +12,8 @@
             var secondPlayerPosition = Console.WindowHeight / 2 - secondPlayerBoardSize / 2;
             var firstPlayerResult = 0;
             var secondPlayerResult = 0;
+            var ballDirectionUp = true;
+            var ballDirectionRight = true;
 
             ConsoleSettings();
             var drawer = new Drawer(new Writer());
@@ -19,6 +21,8 @@
             {
                 firstPlayerPosition = Controller.FirstPlayerMove(firstPlayerPosition, firstPlayerBoardSize);
                 secondPlayerPosition = Controller.SecondPlayerAIMove(secondPlayerPosition, secondPlayerBoardSize);
+                Controller.MoveBall(firstPlayerBoardSize, secondPlayerBoardSize, ref ballPositionX, ref ballPositionY, firstPlayerPosition, secondPlayerPosition, ref firstPlayerResult, ref secondPlayerResult, ref ballDirectionUp, ref ballDirectionRight);
+
                 Console.Clear();
                 drawer.DrawFirstPlayer(firstPlayerPosition, firstPlayerBoardSize);
                 drawer.DrawSecondPlayer(secondPlayerPosition, secondPlayerBoardSize);
@@ -27,6 +31,7 @@
                 Thread.Sleep(60);
             }
         }
+     
 
         private static void ConsoleSettings()
         {
